0896e9309987729dbe036a2080070e37ad9f781f
herve.ngisse <hervembl3@gmail.com>
2023-04-15 20:43:46 -0600

    Update ServerBonusLauncher.java

--- src/main/java/server/ServerBonusLauncher.java
+++ src/main/java/server/ServerBonusLauncher.java
@@ -4,43 +4,45 @@ import java.io.IOException;
 import java.net.ServerSocket;
 
 public class ServerBonusLauncher {
-    static ServerSocket serverSocket; // 0 permet au système de choisir un port libre
-
-    static {
-        int attempts = 0;
-        while (attempts < 10) { // Limite le nombre de tentatives pour éviter une boucle infinie
+    private static final int MIN_PORT = 9090;
+    private static final int MAX_PORT = 9095;
+    public static int findFreePort() throws IOException {
+        int port = -1;
+        for (int i = MIN_PORT; i <= MAX_PORT; i++) {
             try {
-                serverSocket = new ServerSocket(0);
-                break; // Sort de la boucle si la création de ServerSocket réussit
+                System.out.println(i);
+                ServerSocket socket = new ServerSocket(i);
+                socket.close();
+                port = i;
+                break;
             } catch (IOException e) {
-                attempts++;
+                // Port already in use, continue searching
+                continue;
             }
         }
+        if (port == -1) {
+            throw new IOException("Could not find a free port between " + MIN_PORT + " and " + MAX_PORT);
+        }
+        return port;
     }
-
-    static int PORT = serverSocket.getLocalPort(); // Récupérer le port choisi par le système
-
     /**
      * Le port utilisé pour le serveur
      */
-    //public final static int PORT = 9090;
-
-    public ServerBonusLauncher() throws IOException {
-        Server server;
-        try {
-            server = new Server(PORT);
-            System.out.println("Server is running...");
-            server.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public final static int PORT = 9090;
 
     /**
      * Démarre le serveur sur un port donné et l'exécute.
      * @param arg un tableau d'arguments de ligne de commande (non-utilisé)
      */
-    public static void main(String[] arg) throws IOException {
-        new ServerLauncher();
+    public static void main(String[] arg) {
+        Server server;
+        try {
+            int port = findFreePort();
+            server = new Server(port);
+            System.out.println("Server is running...");
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

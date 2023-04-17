ecf01b2992502522d99fd6a221dd82ed7e0340fe
herve.ngisse <hervembl3@gmail.com>
2023-04-17 10:19:36 -0600

    javadoc client_fx

---
+++ javadoc_client_fx/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"client.ClientFX","l":"InscriptionController"},{"p":"client.ClientFX","l":"InscriptionLauncher"},{"p":"client.ClientFX","l":"InscriptionView"}];updateSearchResults();
